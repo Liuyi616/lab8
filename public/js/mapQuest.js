140e64c629d711adfef975d2b8d30ee550fc307e
Liuyi616 <lion61ly@gmail.com>
2021-02-25 19:35:54 -0800

    edition-2.25

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = "zShEeE1KeaYAuA5tZeXO7iw1SZClhOgf";
+
+	//'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	//add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
